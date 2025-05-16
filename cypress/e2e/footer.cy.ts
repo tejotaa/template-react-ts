7dd65969532542c0f958baf2b65ba0e9254e1102
tejotaa <tjn1993@gmail.com>
2025-05-16 22:59:38 +0200

    adjust e2e

--- cypress/e2e/footer.cy.ts
+++ cypress/e2e/footer.cy.ts
@@ -12,23 +12,20 @@ describe('Footer actions', () => {
     cy.findByText('2025 Bakery').should('exist').click();
     cy.url().should('include', '/');
     // language selector
-    cy.findByText('English').should('not.exist');
+    cy.findByText('ENGLISH').should('not.exist');
     cy.findByTestId('LanguageIcon').should('exist');
     cy.findByTestId('language').should('exist').click();
-    cy.findByText('English').should('exist').click();
-    cy.findByText('English').should('exist');
+    cy.findByText('ENGLISH').should('exist').click();
+    cy.findByText('ENGLISH').should('be.visible');
     // currency selector
     cy.findByText('USD').should('not.exist');
     cy.findByTestId('AttachMoneyIcon').should('exist');
     cy.findByTestId('currency').should('exist').click();
     cy.findByText('USD').should('exist').click();
-    cy.findByText('USD').should('exist');
+    cy.findByText('USD').should('be.visible');
     // social icons
-    cy.findByAltText('Facebook').should('exist');
     cy.findByTestId('social-icon-facebook').should('exist');
-    cy.findByAltText('Twitter').should('exist');
     cy.findByTestId('social-icon-twitter').should('exist');
-    cy.findByAltText('Instagram').should('exist');
     cy.findByTestId('social-icon-instagram').should('exist');
   });
 });
